4796a67e6111f13edec4cfa4541f505eb9083625
Fabricio-nav <00038325@uca.edu.sv>
2025-05-06 10:34:02 -0600

    Ejercicio_Fabricio_00038325.cpp

---
+++ Ejercicio_Fabricio_00038325.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void){
+    int num;
+
+    cout << "ingrese un numero: \n";
+    cin >> num;
+
+    if (num % 2 == 0){
+        cout << "el numero no es impar \n";
+    } else{
+        cout << "el numero es impar \n";
+    }
+        
+
+    
+
+
+
+
+
+
+
+
+    return 0;
+}
